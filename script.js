120ce36a952477206c188504aa498340464002a2
Hcran18 <112521786+Hcran18@users.noreply.github.com>
2022-10-14 11:06:39 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+document.getElementById("movieSubmit").addEventListener("click", function(event) {
+  event.preventDefault();
+  const value = document.getElementById("movieInput").value;
+  if (value === "")
+    return;
+  console.log(value);
+  const url = "https://imdb-api.com/en/API/SearchMovie/k_3aqst5sb/" + value;
+  var ID;
+  fetch(url)
+    .then(function(response) {
+      return response.json();
+    }).then(function(json) {
+        ID = json.results[0].id;
+        console.log(ID);
+        const url2 = "https://imdb-api.com/en/API/FullCast/k_3aqst5sb/" + ID;
+        
+      let results = "";
+      
+      results += "<h2>" + json.results[0].title; + "<h2/>";
+      results += "<div></div>";
+      results += "<img src=" + json.results[0].image + ">";
+     
+      document.getElementById("movieResults").innerHTML = results;
+      
+    fetch(url2)
+    .then(function(response) {
+        return response.json();
+    }).then(function(json) {	
+        let results = "";
+        
+        results += "<h2>" + json.directors.job + "</h2>";
+        for (let i = 0; i < json.directors.items.length; i++) {
+            results += "<p>" + json.directors.items[i].name + "</p>"
+            
+        }
+        document.getElementById("movieResults").innerHTML += results;
+        results = "";
+        
+        results += "<h2>Cast</h2>";
+        
+        for (let i = 0; i < json.actors.length; i++) {
+            results += "<p>" + json.actors[i].name + " -- " + json.actors[i].asCharacter + "<p/>";
+            
+        }
+        document.getElementById("movieResults").innerHTML += results;
+    })
+    .catch(err => alert(err));
+    })
+    .catch(err => alert(err));
+    
+    
+    
+});
